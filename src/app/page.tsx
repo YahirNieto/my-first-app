aa041c16dc35565113ac1ae0187c326a7a2de4a4
Yahir Nieto <ynieto9@gmail.com>
2025-06-18 13:14:55 -0600

    primer commit

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-slate-900 text-white">
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-4">¡Hola, mundo! 🌎</h1>
+        <p className="text-lg">Tu primera app con Next.js ya está corriendo 🚀</p>
+      </div>
+    </main>
+  );
+}
